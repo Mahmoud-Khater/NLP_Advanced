382e2e95b19db4236e1bf179e639ae3a5fe1fc6b
Mohamedraggab <45504628+Mohamedraggab@users.noreply.github.com>
2024-11-08 15:13:26 +0200

    test

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,124 +1,124 @@
 #!/bin/bash
 
-pip install -r /kaggle/working/NLP_Advanced/scripts/requirements.txt --quiet
+pip install -r /content/NLP_Advanced/scripts/requirements.txt --quiet
 
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "microsoft/mdeberta-v3-base" \
         --batch_size 16 \
         --num_train_epochs 10
 : <<'EOF'
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "microsoft/mdeberta-v3-base" \
         --batch_size 64 \
         --num_train_epochs 10
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "bert-base-multilingual-cased" \
         --batch_size 128 \
         --num_train_epochs 10
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "bert-base-multilingual-cased" \
         --batch_size 64 \
         --num_train_epochs 10
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "hiiamsid/sentence_similarity_spanish_es" \
         --batch_size 128 \
         --num_train_epochs 10 \
         --language "esp"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "hiiamsid/sentence_similarity_spanish_es" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "esp"
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "l3cube-pune/marathi-sentence-similarity-sbert" \
         --batch_size 128 \
         --num_train_epochs 10 \
         --language "mar"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "l3cube-pune/marathi-sentence-similarity-sbert" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "mar"
  # -------------------------------------------------       
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "l3cube-pune/telugu-sentence-similarity-sbert" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "tel"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "l3cube-pune/telugu-sentence-similarity-sbert" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "tel"
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "mbeukman/xlm-roberta-base-finetuned-kinyarwanda-finetuned-ner-swahili" \
         --batch_size 128 \
         --num_train_epochs 10 \
         --language "kin"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "mbeukman/xlm-roberta-base-finetuned-kinyarwanda-finetuned-ner-swahili" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "kin"
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "microsoft/deberta-v3-large" \
         --batch_size 128 \
         --num_train_epochs 10 \
         --language "eng"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "microsoft/deberta-v3-large" \
         --batch_size 64 \
         --num_train_epochs 10 \
         --language "eng"
 # -------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7" \
         --batch_size 128 \
         --num_train_epochs 10 \
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7" \
         --batch_size 64 \
         --num_train_epochs 10 \
 # intfloat/multilingual-e5-base
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "intfloat/multilingual-e5-base" \
         --batch_size 128 \
         --num_train_epochs 10 \
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "intfloat/multilingual-e5-base" \
         --batch_size 64 \
         --num_train_epochs 10 \
 # intfloat/multilingual-e5-base
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "intfloat/multilingual-e5-large" \
         --batch_size 128 \
         --num_train_epochs 10 \
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "intfloat/multilingual-e5-large" \
         --batch_size 64 \
         --num_train_epochs 10 \
 # --------------------------------------------------
-python /kaggle/working/NLP_Advanced/scripts/train_mlm.py \
+python /content/NLP_Advanced/scripts/train_mlm.py \
         --model_name "intfloat/e5-large-v2" \
         --batch_size 128 \
         --num_train_epochs 10 \
         --language "eng"
 
-python /kaggle/working/NLP_Advanced/scripts/train_ce.py \
+python /content/NLP_Advanced/scripts/train_ce.py \
         --model_name "intfloat/e5-large-v2" \
         --batch_size 64 \
         --num_train_epochs 10 \
